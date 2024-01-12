4d3996bc7183453e9483788d1908db54de3e39a3
John Bartmann <pravda23@gmail.com>
2024-01-12 13:26:44 +0200

    toggle cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,20 +7,16 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
 
 const Nav = () => {
-  const [cartIsShowing, setCartIsShowing] = useState(false);
-
-  // const showCartClickHandler = () => {
-  //   console.log("hello");
-  //   // setCartIsShowing(!cartIsShowing);
-  // };
-
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+
   return (
     <Fragment>
       <div className="navigation">
@@ -45,7 +41,7 @@ const Nav = () => {
 
           <CartIcon />
         </div>
-        <CartDropdown />
+        {isCartOpen && <CartDropdown />}
       </div>
 
       <Outlet />
