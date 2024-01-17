ab03ce4e5ad181d81a127d4ef43e31435ce8f890
John Bartmann <pravda23@gmail.com>
2024-01-17 09:57:21 +0200

    added instructional method for batch writing SHOP_DATA to firebase (once-off useEffect running addCollectionAndDocuments)

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,9 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.jsx";
+
+// only required for useEffect learning purposes - see below
+// import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
   products: [],
@@ -8,7 +11,15 @@ export const ProductsContext = createContext({
 
 // every created context requires its own provider
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  // below useEffect is a once-off method of writing data to firebase; commented out to avoid rerunning with every refresh, but retained for learning purposes; normally not done from front end
+
+  // useEffect(() => {
+  //   // function sets the desired category name to 'categories'
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -9,7 +9,14 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAqWsG1hkE9t97_WJPlKmAaX4WCkZ6o8-4",
@@ -31,11 +38,35 @@ googleProvider.setCustomParameters({
 
 // creates user auth; sets display name, email and date
 export const auth = getAuth();
-export const signInWithGooglePopup = () => {
-  return signInWithPopup(auth, googleProvider);
-};
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+// collectionKey is firebase category name, eg "users"
+// objectsToAdd is the documents to add
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  // each object passed is an item in the SHOP_DATA array
+  objectsToAdd.forEach((object) =>
+    // collectionRef returned when collection method is called on db, now we use it to write objects
+    {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+
+      // set will pass the value of the object and write object contents to the location set by collectionRef
+      batch.set(docRef, object);
+    }
+  );
+  await batch.commit();
+  console.log("done");
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
@@ -82,7 +113,7 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
   return await signInWithEmailAndPassword(auth, email, password);
 };
 
-export const signOutUser = async () => signOut(auth);
+export const signOutUser = async () => await signOut(auth);
 
 // creates a listener using the provided callback
 export const onAuthStateChangedListener = (callback) =>
